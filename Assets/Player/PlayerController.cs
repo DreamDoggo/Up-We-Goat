072b798f8249074f9bbe7e95ba2e0ea39661e273
DreamDoggo <doggosanddragons@gmail.com>
2024-07-22 15:54:48 -0700

    saved new scene

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float MoveSpeed;  
+
     // Start is called before the first frame update
     void Start()
     {
