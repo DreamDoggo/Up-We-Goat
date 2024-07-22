3a2e52acbbacdcb1ea0e190309ceae882243cbfc
DreamDoggo <doggosanddragons@gmail.com>
2024-07-22 15:58:09 -0700

    e

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float MoveSpeed;  
+    [SerializeField] KeyCode MoveLeftKey;
+    [SerializeField] float MoveSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void UpdateMovement() 
+    {
+        if (Input.GetKey)
     }
 }
