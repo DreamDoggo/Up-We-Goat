daafdf1dd1dae0c0b9c6cc2bf27d1283914e8ed5
GregSherr <gregsherris@gmail.com>
2024-07-23 17:35:09 -0400

    change

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -111,7 +112,7 @@ public class PlayerController : MonoBehaviour
     void OnTriggerEnter2D(Collider2D coll){
         if (coll.tag == HazardTag)
         {
-            Destroy(gameObject);
+            SceneManager.LoadScene("StartMenu");
         }
     }
 }
