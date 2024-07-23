109bdae255958df3166eb3548f5acc512714c26a
RatL0rd <rlsea08@gmail.com>
2024-07-23 16:55:46 -0700

    screens

--- Assets/ButtonManager.cs
+++ Assets/ButtonManager.cs
@@ -3,18 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ButtonManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
+    public void StartGame()
+    {
+        SceneManager.LoadScene("sketch-leap");
+    }
+
+    public void GotToTips()
+    {
+        SceneManager.LoadScene("TipsScreen");
+    }
+
+    public void GoToMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
 }
