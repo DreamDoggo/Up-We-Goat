7a3f8ee3e8db574d5c5c1cb0b0eddc174db3f807
DreamDoggo <doggosanddragons@gmail.com>
2024-07-26 16:17:40 -0700

    e

--- Assets/levelManager.cs
+++ Assets/levelManager.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class levelManager : MonoBehaviour
+public class LevelManager : MonoBehaviour
 {
     [Tooltip("The level which the player is currently in")]
-    [SerializeField] public static int level = 0;
+    [SerializeField] public static int Level = 0;
+
     void OnTriggerEnter2D(Collider2D coll){
         if (coll.gameObject.name == "Player"){
-            level++;
+            Level++;
             Destroy(gameObject);
         }
     }
+
 }
