a7624389249c16692dc297d7c4151b7149989085
GregSherr <gregsherris@gmail.com>
2024-07-25 21:54:04 -0400

    Background instantiation challenge (IMPOSSIBLE)

--- Assets/Backgrounds/BackgroundManager.cs
+++ Assets/Backgrounds/BackgroundManager.cs
@@ -1,29 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class BackgroundManager : MonoBehaviour
 {
-    [SerializeField] Texture2D BG_Test1;
-    [SerializeField] Texture2D BG_Test2;
-    [SerializeField] Vector2 SpawnPosition;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject[] Backgrounds = new GameObject[4];
 
-    SpriteRenderer Sprite;
+    [Tooltip("How high the player needs to be before initiating the infinite spawing backgrounds")]
+    [SerializeField] float infiniteBegin = 0;
 
-    private void Awake()
-    {
-        Sprite = GetComponent<SpriteRenderer>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [Tooltip("How high the first backdrop is placed")]
+    [SerializeField] float infinitePlacementBegin = 0;
+    
+    [SerializeField] int numberOfDupes = 0;
+    float colliderHolder = 0;
     void Update()
     {
-        
+        if (player.transform.position.y >= infiniteBegin + colliderHolder * numberOfDupes){
+            int rand = UnityEngine.Random.Range(0,Backgrounds.Length+1);
+            BoxCollider2D backgroundCollider = Backgrounds[rand].GetComponent<BoxCollider2D>();
+            Instantiate(Backgrounds[rand], new Vector3(0, infinitePlacementBegin + colliderHolder, 0), Quaternion.identity);
+            colliderHolder = colliderHolder + backgroundCollider.size.y;
+            numberOfDupes++;
+        }
+
     }
 }
